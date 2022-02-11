a9de7d5940bcdd3a6eaade473405e7854faa3a5d
kampersanda <shnsk.knd@gmail.com>
2022-02-11 14:17:30 +0000

    add prob record

--- tongrams/src/record.rs
+++ tongrams/src/record.rs
@@ -1,3 +1,5 @@
+use float_cmp::ApproxEq;
+
 use crate::Gram;
 
 /// Handler of a pair of a gram and its count.
@@ -26,3 +28,51 @@ impl CountRecord {
         self.count
     }
 }
+
+/// Handler of a tuple of a gram, its probability, and its backoff weight.
+#[derive(Clone, Debug)]
+pub struct ProbRecord {
+    gram: String, // TODO: Store as a byte slice to another buffer
+    prob: f32,
+    backoff: f32,
+}
+
+impl ProbRecord {
+    /// Creates a [`ProbRecord`].
+    #[inline]
+    pub const fn new(gram: String, prob: f32, backoff: f32) -> Self {
+        Self {
+            gram,
+            prob,
+            backoff,
+        }
+    }
+
+    /// Gets the [`Gram`].
+    #[inline]
+    pub fn gram(&self) -> Gram {
+        Gram::new(self.gram.as_bytes())
+    }
+
+    /// Gets the probability.
+    #[inline]
+    pub const fn prob(&self) -> f32 {
+        self.prob
+    }
+
+    /// Gets the backoff weight.
+    #[inline]
+    pub const fn backoff(&self) -> f32 {
+        self.backoff
+    }
+}
+
+impl PartialEq for ProbRecord {
+    fn eq(&self, other: &Self) -> bool {
+        self.gram == other.gram
+            && self.prob.approx_eq(other.prob, (0.0, 2))
+            && self.backoff.approx_eq(other.backoff, (0.0, 2))
+    }
+}
+
+impl Eq for ProbRecord {}
