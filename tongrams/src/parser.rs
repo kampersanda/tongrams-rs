f048fc2e6dee3f80e77d861c50f88cb27a1a6c8f
kampersanda <shnsk.knd@gmail.com>
2022-02-11 14:46:44 +0000

    rename

--- tongrams/src/parser.rs
+++ tongrams/src/parser.rs
@@ -107,29 +107,29 @@ where
 mod tests {
     use super::*;
 
-    const GRAMS_1: &'static str = "4
+    const COUNT_GRAMS_1: &'static str = "4
 A\t10
 B\t7
 C\t4
 D\t1
 ";
 
-    const GRAMS_2: &'static str = "4
+    const COUNT_GRAMS_2: &'static str = "4
 A A\t1
 A C\t2
 B B\t3
 D C\t1
 ";
 
-    const GRAMS_3: &'static str = "3
+    const COUNT_GRAMS_3: &'static str = "3
 A A C\t2
 B B C\t1
 D D D\t1
 ";
 
     #[test]
-    fn test_grams_1() {
-        let mut gp = GramsParser::new(BufReader::new(GRAMS_1.as_bytes())).unwrap();
+    fn test_count_grams_1() {
+        let mut gp = GramsParser::new(BufReader::new(COUNT_GRAMS_1.as_bytes())).unwrap();
         assert_eq!(gp.num_grams(), 4);
         for (gram, count) in [("A", 10), ("B", 7), ("C", 4), ("D", 1)] {
             let gram = gram.to_string();
@@ -142,8 +142,8 @@ D D D\t1
     }
 
     #[test]
-    fn test_grams_2() {
-        let mut gp = GramsParser::new(BufReader::new(GRAMS_2.as_bytes())).unwrap();
+    fn test_count_grams_2() {
+        let mut gp = GramsParser::new(BufReader::new(COUNT_GRAMS_2.as_bytes())).unwrap();
         assert_eq!(gp.num_grams(), 4);
         for (gram, count) in [("A A", 1), ("A C", 2), ("B B", 3), ("D C", 1)] {
             let gram = gram.to_string();
@@ -156,8 +156,8 @@ D D D\t1
     }
 
     #[test]
-    fn test_grams_3() {
-        let mut gp = GramsParser::new(BufReader::new(GRAMS_3.as_bytes())).unwrap();
+    fn test_count_grams_3() {
+        let mut gp = GramsParser::new(BufReader::new(COUNT_GRAMS_3.as_bytes())).unwrap();
         assert_eq!(gp.num_grams(), 3);
         for (gram, count) in [("A A C", 2), ("B B C", 1), ("D D D", 1)] {
             let gram = gram.to_string();
