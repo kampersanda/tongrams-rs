8f0fd1224bebad8f07ac8ad86556982c7d24c3db
kampersanda <shnsk.knd@gmail.com>
2022-02-12 19:40:17 +0000

    add from func

--- tongrams/src/trie_prob_lm.rs
+++ tongrams/src/trie_prob_lm.rs
@@ -1,5 +1,13 @@
 mod builder;
 
+use anyhow::Result;
+
+use crate::loader::{GramsLoader, GramsTextLoader};
+use crate::trie_array::TrieArray;
+use crate::vocabulary::Vocabulary;
+
+pub use builder::TrieProbLmBuilder;
+
 /// Elias-Fano trie for indexing *N*-grams with their frequency counts.
 #[derive(Default, Debug)]
 pub struct TrieProbLm<T, V> {
@@ -7,4 +15,21 @@ pub struct TrieProbLm<T, V> {
     arrays: Vec<T>,
     probs: Vec<Vec<f32>>,    // TODO: Quantize
     backoffs: Vec<Vec<f32>>, // TODO: Quantize
+}
+
+impl<T, V> TrieProbLm<T, V>
+where
+    T: TrieArray,
+    V: Vocabulary,
+{
+    /// Builds the index from *N*-gram models of raw texts (for debug).
+    #[doc(hidden)]
+    pub fn from_texts(texts: Vec<&'static str>) -> Result<Self> {
+        let mut loaders = Vec::with_capacity(texts.len());
+        for text in texts {
+            let loader: Box<dyn GramsLoader<_>> = Box::new(GramsTextLoader::new(text.as_bytes()));
+            loaders.push(loader);
+        }
+        TrieProbLmBuilder::new(loaders)?.build()
+    }
 }
